fe7fc507a01dcfc77db00a5ca50477167eb60d1f
kimpeter2018 <kimpeter2018@gmail.com>
2022-12-29 17:24:59 +0900

    make typo

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Disko Demo',
+      title: 'Disko Dem',
 
       theme: ThemeData(
         primarySwatch: Colors.blue,

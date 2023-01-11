ba53c71ae524e994985176f5acc5ed214e84165b
kimpeter2018 <kimpeter2018@gmail.com>
2023-01-11 21:16:41 +0900

    add Landing Pages

---
+++ lib/screens/starting_screens/landing_pages/pages/first_landing_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class FirstLandingPage extends StatefulWidget {
+  const FirstLandingPage({Key? key}) : super(key: key);
+
+  @override
+  State<FirstLandingPage> createState() => _FirstLandingPageState();
+}
+
+class _FirstLandingPageState extends State<FirstLandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(27.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  '낯선 곳에서',
+                  style: TextStyle(fontWeight: FontWeight.w700, fontSize: 38),
+                ),
+                const Text(
+                  '낯익은 우리',
+                  style: TextStyle(fontWeight: FontWeight.w700, fontSize: 38),
+                ),
+                const Text(
+                  '하나가 되는 순간!',
+                  style: TextStyle(fontWeight: FontWeight.w700, fontSize: 38),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Image.asset('assets/disco-ball-shiny.gif'),
+                SizedBox(
+                  height: 105,
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  '한인 대표 커뮤니티',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16,
+                      color: Theme.of(context).colorScheme.primary),
+                ),
+                Text(
+                  'DISKO',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w800,
+                      fontSize: 40,
+                      color: Theme.of(context).colorScheme.primary),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}

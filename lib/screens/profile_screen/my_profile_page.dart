95265efbf15a6d907aaaad53c5c3ae350bb49cba
kimpeter2018 <kimpeter2018@gmail.com>
2023-01-19 13:28:23 +0900

    Personalize Profile Page

---
+++ lib/screens/profile_screen/my_profile_page.dart
@@ -0,0 +1,54 @@
+import 'package:disko_001/screens/profile_screen/profile_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import '../../src/tools.dart';
+
+class MyProfilePage extends StatefulWidget {
+  const MyProfilePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyProfilePage> createState() => _MyProfilePageState();
+}
+
+class _MyProfilePageState extends State<MyProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: getDisplayNameByUid(FirebaseAuth.instance.currentUser!.uid),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasData == false) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          return Scaffold(
+            body: ProfilePage(
+              displayName: snapshot.data.toString(),
+              country: '이스라엘',
+              description: '안녕하세요! 이스라엘 거주중 엥뿌삐 올리비아 입니다',
+            ),
+          );
+        });
+  }
+
+  Widget mirrorballbuilder() {
+    return Container(
+      child: const Text('mirrorball'),
+    );
+  }
+
+  Widget Mypost() {
+    return Container(
+      child: const Text('My post'),
+    );
+  }
+
+  Widget QandA() {
+    return Container(
+      child: const Text('Q and A'),
+    );
+  }
+
+  Widget Followings() {
+    return const Text('Followings');
+  }
+}

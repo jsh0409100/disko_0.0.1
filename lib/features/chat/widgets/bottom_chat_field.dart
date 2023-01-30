3fe7e9ec43b951fa164dd3f7f5ee6aa8165f6cf4
kimpeter2018 <kimpeter2018@gmail.com>
2023-01-30 16:09:09 +0900

    add new features in chat

--- lib/features/chat/widgets/bottom_chat_field.dart
+++ lib/features/chat/widgets/bottom_chat_field.dart
@@ -1,7 +1,13 @@
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_sound/public/flutter_sound_recorder.dart';
+import 'package:path_provider/path_provider.dart';
 
+import '../../../common/enums/message_enum.dart';
+import '../../../common/utils/utils.dart';
 import '../controller/chat_controller.dart';
 
 class BottomChatField extends ConsumerStatefulWidget {
@@ -15,25 +21,113 @@ class BottomChatField extends ConsumerStatefulWidget {
 
 class _SendMessageState extends ConsumerState<BottomChatField> {
   final controller = TextEditingController();
+  bool isShowSendButton = false;
+  FlutterSoundRecorder? _soundRecorder;
+  bool isRecorderInit = false;
+  bool isShowEmojiContainer = false;
+  bool isRecording = false;
+  FocusNode focusNode = FocusNode();
   final user = FirebaseAuth.instance.currentUser;
 
   var _userEnterMessage = '';
 
   void _sendMessage() async {
-    ref.read(chatControllerProvider).sendTextMessage(
-          context,
-          _userEnterMessage,
-          widget.receiverUid,
+    if (isShowSendButton) {
+      ref.read(chatControllerProvider).sendTextMessage(
+            context,
+            _userEnterMessage,
+            widget.receiverUid,
+          );
+      controller.clear();
+    } else {
+      var tempDir = await getTemporaryDirectory();
+      var path = '${tempDir.path}/flutter_sound.aac';
+      if (!isRecorderInit) {
+        return;
+      }
+      if (isRecording) {
+        await _soundRecorder!.stopRecorder();
+        sendFileMessage(File(path), MessageEnum.audio);
+      } else {
+        await _soundRecorder!.startRecorder(
+          toFile: path,
         );
-    controller.clear();
+      }
 
-    // final currentChat =
-    //     FirebaseFirestore.instance.collection('messages').doc(widget.chatName);
-    // final doc = await currentChat.get();
-    // (doc.exists)
-    //     ? currentChat.update(messageData)
-    //     : currentChat.set(messageData);
-    // await currentChat.update({"unreadMessageCount": FieldValue.increment(1)});
+      setState(() {
+        isRecording = !isRecording;
+      });
+    }
+  }
+
+  void sendFileMessage(
+    File file,
+    MessageEnum messageEnum,
+  ) {
+    ref.read(chatControllerProvider).sendFileMessage(
+          context,
+          file,
+          widget.receiverUid,
+          messageEnum,
+        );
+  }
+
+  void selectImage() async {
+    File? image = await pickImageFromGallery(context);
+    if (image != null) {
+      sendFileMessage(image, MessageEnum.image);
+    }
+  }
+
+  void selectVideo() async {
+    File? video = await pickVideoFromGallery(context);
+    if (video != null) {
+      sendFileMessage(video, MessageEnum.video);
+    }
+  }
+
+  void selectGIF() async {
+    final gif = await pickGIF(context);
+    if (gif != null) {
+      ref.read(chatControllerProvider).sendGIFMessage(
+            context,
+            gif.url,
+            widget.receiverUid,
+          );
+    }
+  }
+
+  void hideEmojiContainer() {
+    setState(() {
+      isShowEmojiContainer = false;
+    });
+  }
+
+  void showEmojiContainer() {
+    setState(() {
+      isShowEmojiContainer = true;
+    });
+  }
+
+  void showKeyboard() => focusNode.requestFocus();
+  void hideKeyboard() => focusNode.unfocus();
+
+  void toggleEmojiKeyboardContainer() {
+    if (isShowEmojiContainer) {
+      showKeyboard();
+      hideEmojiContainer();
+    } else {
+      hideKeyboard();
+      showEmojiContainer();
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+    _soundRecorder!.closeRecorder();
+    isRecorderInit = false;
   }
 
   @override
--- lib/models/chat_message_model.dart
+++ lib/models/chat_message_model.dart
@@ -1,26 +1,35 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import '../common/enums/message_enum.dart';
+
 class ChatMessageModel {
-  final String senderId, text, receiverUid;
+  final String senderId, text, receiverUid, messageId;
   final Timestamp timeSent;
+  final MessageEnum type;
 
   ChatMessageModel({
     required this.timeSent,
     required this.senderId,
     required this.text,
     required this.receiverUid,
+    required this.type,
+    required this.messageId,
   });
 
   ChatMessageModel.fromJson(Map<String, dynamic> json)
       : senderId = json['senderId'],
         text = json['text'],
         timeSent = json['timeSent'],
-        receiverUid = json['receiverUid'];
+        type = json['type'],
+        receiverUid = json['receiverUid'],
+        messageId = json['messageId'];
 
   Map<String, dynamic> toJson() => {
         'senderId': senderId,
         'text': text,
         'timeSent': timeSent,
+        'messageId': messageId,
+        'type': type,
         'receiverUid': receiverUid
       };
 }

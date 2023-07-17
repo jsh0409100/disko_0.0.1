0ab7b8c13c2b61271837ee834c4f28e6d517e84b
bangeunchan1593 <bang9911@gmail.com>
2023-07-17 17:48:03 +0900

    Detail -> OtherProfilePage

--- lib/router.dart
+++ lib/router.dart
@@ -14,6 +14,7 @@ import 'features/chat/screens/chat_screen.dart';
 import 'features/report/report_screen.dart';
 import 'features/search/screens/search.dart';
 import 'features/write_post/screens/write_post_page.dart';
+import 'models/post_card_model.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
@@ -35,9 +36,9 @@ Route<dynamic> generateRoute(RouteSettings settings) {
       );
     case DetailPage.routeName:
       final arguments = settings.arguments as Map<String, dynamic>;
-      final postId = arguments['postId'];
+      final PostCardModel post = arguments['post'];
       return MaterialPageRoute(
-        builder: (context) => DetailPage(postId: postId),
+        builder: (context) => DetailPage(post: post),
       );
     case TestScreen.routeName:
       final arguments = settings.arguments as Map<String, dynamic>;
